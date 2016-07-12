08270eb0966c8dae04b619443d8f0de7ce004f20
Chris <chrisyao19@gmail.com>
2016-07-12 20:21:26 +0800

    Valid Parentheses

---
+++ String/20/Stack.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean isValid(String s) {
+        String str = "({[";
+        Stack<Character> stack = new Stack<Character>();
+        for(int i=0; i < s.length();i++) {
+            if(str.contains(s.charAt(i)+"")) {
+                stack.push(s.charAt(i));
+            } 
+            else if(s.charAt(i)==')' && !stack.empty() && stack.peek()=='(') stack.pop();
+            else if(s.charAt(i)==']' && !stack.empty() && stack.peek()=='[') stack.pop();
+            else if(s.charAt(i)=='}' && !stack.empty() && stack.peek()=='{') stack.pop();
+            else return false;
+        }
+        return stack.empty();
+      
+        
+    }
+}
